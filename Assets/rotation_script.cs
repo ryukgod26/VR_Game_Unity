92131ce2ff302ea75fb02bcf56c26833a2ec5a24
raviwhitehat <126102004+raviwhitehat@users.noreply.github.com>
2025-08-14 13:09:56 +0530

    added Interactable objects and new design of Player hands

---
+++ Assets/rotation_script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation_script : MonoBehaviour
+{
+    //[Header("Rotation Settings")]
+    //public Vector3 rotationAxis = Vector3.forward;
+    //public float rotationSpeed = 100f;
+
+    //private RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rectTransform = GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Rotating...");
+
+        //transform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime *5f);
+        //rectTransform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime);
+        //GetComponent<RectTransform>().Rotate(rotationAxis * rotationSpeed * Time.deltaTime);
+    }
+}
